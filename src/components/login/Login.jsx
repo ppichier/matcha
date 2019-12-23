e7a5b9ab68d4d3f70120fcc3c93d462634b8c88b
Wafae RHARRABTI <warharra@e2r8p3.42.fr>
2019-12-23 18:54:20 +0100

    [TEST]

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Login.css";
 
 function Login() {
-  return <div className="Login">Je suis login</div>;
+  return <div className="Login"><div>dwddw</div></div>;
 }
 
 export default Login;
