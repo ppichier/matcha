a0a412e94bb62a2796b1c3e4b14b55b19208d5a8
Wafae RHARRABTI <warharra@e2r7p22.42.fr>
2020-01-02 17:12:45 +0100

    [ADD] Routes login, basic signin signup button

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,9 @@
 import React from "react";
-<<<<<<< HEAD
 import "./App.css";
 import Login from "./components/login/Login";
 
 function App() {
   return <div className="App"></div>;
 }
-=======
-import Signup from "./components/Signup";
-import "./App.css";
-
-const App = () => {
-  return (
-    <div>
-      <Signup />
-    </div>
-  );
-};
->>>>>>> [ADD] Routes login, basic signin signup button
 
 export default App;
