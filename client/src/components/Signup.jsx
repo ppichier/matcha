cba07c153eb0a8e2b3e5b61b846dfbb980bd6713
Wafae RHARRABTI <warharra@e2r7p22.42.fr>
2020-01-02 19:42:45 +0100

    [ADD]signup form. [WIP] link state to input

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,8 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Signup.css";
 
 const Signup = () => {
-  return <div className="signup">Signup COMPONENT</div>;
+  const [values, setValues] = useState({
+    email: "",
+    pseudo: "",
+    firstName: "",
+    lastName: "",
+    password: ""
+  });
+
+  const handleChange = event => {
+    const tmp = { ...values, email: event.target.value };
+    setValues(tmp);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+  return (
+    <div className="signup">
+      <form>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            onChange={handleChange}
+            type="email"
+            className="form-control"
+          ></input>
+        </div>
+        <div className="form-group">
+          <label>Pseudo</label>
+          <input
+            onChange={handleChange}
+            type="text"
+            className="form-control"
+          ></input>
+        </div>
+        <div className="form-group">
+          <label>Nom</label>
+          <input
+            onChange={handleChange}
+            type="text"
+            className="form-control"
+          ></input>
+        </div>
+        <div className="form-group">
+          <label>Prenom</label>
+          <input
+            onChange={handleChange}
+            type="text"
+            className="form-control"
+          ></input>
+        </div>
+        <div className="form-group">
+          <label>Mot de passe</label>
+          <input
+            onChange={handleChange}
+            type="password"
+            className="form-control"
+          ></input>
+        </div>
+        <button onClick={handleSubmit} className="btn btn-primary">
+          S'inscrire
+        </button>
+      </form>
+      {JSON.stringify(values)}
+    </div>
+  );
 };
 
 export default Signup;
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Routes from "./Routes";
 import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
 
