eb8d887d2a3521a5e44e5178ee04f7d805d84908
Wafae RHARRABTI <warharra@e2r9p18.42.fr>
2020-01-03 14:45:49 +0100

    [DEl] service worker , logo

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Routes from "./Routes";
-import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
@@ -10,4 +9,4 @@ ReactDOM.render(<Routes />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
