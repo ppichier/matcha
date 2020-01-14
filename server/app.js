0ab74d14ed4019b54387f1d3cda3b217f6eb203c
ppichier <pierantonio.pichierri@gmail.com>
2020-01-14 16:12:10 +0100

    [ADD] .env file

--- server/app.js
+++ server/app.js
@@ -2,15 +2,18 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.post("/signup", (req, res) => {
   console.log(req.body);
-  res.json({err: "test"});
+  res.json({ err: "test" });
 });
 
-app.listen(8000, () => {
-  console.log("App listen on port 8000");
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`App listen on port ${port}`);
 });
