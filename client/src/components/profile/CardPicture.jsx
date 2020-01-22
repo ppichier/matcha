df2329b8b2e30e7d3599503b9732c19a17ac07c0
Wafae RHARRABTI <warharra@e2r6p13.42.fr>
2020-01-22 18:04:29 +0100

    [Wip] Profile

--- client/src/components/profile/CardPicture.jsx
+++ client/src/components/profile/CardPicture.jsx
@@ -1,21 +1,39 @@
 import React, { useState, Fragment } from "react";
-import { Col } from "react-bootstrap";
-
+import "./CardPicture.css";
+import { Row, Container, Image } from "react-bootstrap";
 const CardPicture = () => {
   return (
     <Fragment>
-      <div>
-        <div className="col-lg-3 col-md-4 wav-left-none">
-          <label htmlFor="single">
-            <img
-              className="test"
-              src="https://img.myloview.fr/images/icone-de-profil-d-espace-reserve-par-defaut-400-81929628.jpg?text=First slide&bg=373940"
-              alt="First slide"
-            />
-          </label>
-          <input type="file" name="file" id="single" />
-        </div>
-      </div>
+      <Container>
+        <Row>
+          <div className="profile-header-container">
+            <div className="profile-header-img">
+              <label htmlFor="profile">
+                <Image
+                  src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120"
+                  roundedCircle
+                />
+              </label>
+              <div className="rank-label-container">
+                <span className="label label-default rank-label">
+                  100♥️♥️♥️
+                </span>
+              </div>
+              <input
+                type="file"
+                name="file"
+                id="profile"
+                // onChange={handleChange}
+              />
+              <div className="info">
+                <a>Wafae Meddah</a>
+                <div className="desc">Age: 30 ans</div>
+                <div className="desc">Paris</div>
+              </div>
+            </div>
+          </div>
+        </Row>
+      </Container>
     </Fragment>
   );
 };
--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -45,178 +45,200 @@ const Profile = () => {
     <Fragment>
       <NavbarHeader />
       <Container>
-        <Row>
-          <Col as={Col} md={3} className="pt-5">
-            ?
+        <Row style={{ flexWrap: "wrap" }}>
+          <Col md={4} className="mt-5 row-pictureProfile">
+            <CardPicture />
           </Col>
-          <Col md={9}>
+          <Col md={8} className="pl-5">
             <Row className="mt-5 row-picture">
               <Picture />
             </Row>
             <Row className="pt-4 row-infos">
               <Col>
-                <Form>
-                  <Form.Row>
-                    <Form.Group as={Col} md="4">
-                      <Form.Label>Nom</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="Nom"
-                        onChange={handleChange("firstName")}
-                      ></Form.Control>
-                    </Form.Group>
-                    <Form.Group as={Col} md="4">
-                      <Form.Label>Prenom</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="Prenom"
-                        onChange={handleChange("lastName")}
-                      ></Form.Control>
-                    </Form.Group>
-                    <Form.Group as={Col} md="4">
-                      <Form.Label>Pseudo</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="Pseudo"
-                        onChange={handleChange("pseudo")}
-                      ></Form.Control>
-                    </Form.Group>
-                  </Form.Row>
-                  <Form.Row>
-                    <Form.Group as={Col} md="6">
-                      <Form.Label>Email</Form.Label>
-                      <Form.Control
-                        type="email"
-                        placeholder="Email"
-                        onChange={handleChange("email")}
-                      ></Form.Control>
-                    </Form.Group>
-                    <Form.Group as={Col} md="6">
-                      <Form.Label>Date de naissance</Form.Label>
-                      <Form.Control
-                        type="date"
-                        placeholder="date de naissance"
-                        name="date"
-                        onChange={handleChange("dateOfBirth")}
-                      ></Form.Control>
-                    </Form.Group>
-                  </Form.Row>
-                  <Form.Row>
-                    <Form.Group as={Col} md="6">
-                      <Form.Label>Ancien Mot de Passe</Form.Label>
-                      <Form.Control
-                        type="password"
-                        placeholder="Ancien mot de passe"
-                        onChange={handleChange("oldPassword")}
-                      ></Form.Control>
-                    </Form.Group>
-                    <Form.Group as={Col} md="6">
-                      <Form.Label>Nouveau Mot de Passe</Form.Label>
-                      <Form.Control
-                        type="password"
-                        placeholder="Nouveau Mot de Passe"
-                        onChange={handleChange("newPassword")}
-                      ></Form.Control>
-                    </Form.Group>
-                  </Form.Row>
-
-                  <Form.Row>
-                    <Form.Group as={Col} md="6">
-                      <Form.Label>Je suis </Form.Label>
-                      <Form.Control
-                        as="select"
-                        onChange={handleChange("gender")}
+                <Row style={{ backgroundColor: "white", marginBottom: "60px" }}>
+                  <Col>
+                    <Form>
+                      <Form.Row>
+                        <Form.Group as={Col} md="4">
+                          <Form.Label>Nom</Form.Label>
+                          <Form.Control
+                            type="text"
+                            placeholder="Nom"
+                            onChange={handleChange("firstName")}
+                          ></Form.Control>
+                        </Form.Group>
+                        <Form.Group as={Col} md="4">
+                          <Form.Label>Prenom</Form.Label>
+                          <Form.Control
+                            type="text"
+                            placeholder="Prenom"
+                            onChange={handleChange("lastName")}
+                          ></Form.Control>
+                        </Form.Group>
+                        <Form.Group as={Col} md="4">
+                          <Form.Label>Pseudo</Form.Label>
+                          <Form.Control
+                            type="text"
+                            placeholder="Pseudo"
+                            onChange={handleChange("pseudo")}
+                          ></Form.Control>
+                        </Form.Group>
+                      </Form.Row>
+                      <Form.Row>
+                        <Form.Group as={Col} md="6">
+                          <Form.Label>Email</Form.Label>
+                          <Form.Control
+                            type="email"
+                            placeholder="Email"
+                            onChange={handleChange("email")}
+                          ></Form.Control>
+                        </Form.Group>
+                        <Form.Group as={Col} md="6">
+                          <Form.Label>Date de naissance</Form.Label>
+                          <Form.Control
+                            type="date"
+                            placeholder="date de naissance"
+                            name="date"
+                            onChange={handleChange("dateOfBirth")}
+                          ></Form.Control>
+                        </Form.Group>
+                      </Form.Row>
+                      <Form.Row>
+                        <Form.Group as={Col} md="6">
+                          <Form.Label>Ancien Mot de Passe</Form.Label>
+                          <Form.Control
+                            type="password"
+                            placeholder="Ancien mot de passe"
+                            onChange={handleChange("oldPassword")}
+                          ></Form.Control>
+                        </Form.Group>
+                        <Form.Group as={Col} md="6">
+                          <Form.Label>Nouveau Mot de Passe</Form.Label>
+                          <Form.Control
+                            type="password"
+                            placeholder="Nouveau Mot de Passe"
+                            onChange={handleChange("newPassword")}
+                          ></Form.Control>
+                        </Form.Group>
+                      </Form.Row>
+                    </Form>
+                  </Col>
+                </Row>
+                <Row style={{ backgroundColor: "white", marginBottom: "60px" }}>
+                  <Col>
+                    <Form>
+                      <Form.Row>
+                        <Form.Group as={Col} md="6">
+                          <Form.Label>Je suis </Form.Label>
+                          <Form.Control
+                            as="select"
+                            onChange={handleChange("gender")}
+                          >
+                            <option value="N"> Séléctionnez un genre</option>
+                            <option value="M"> Un Homme </option>
+                            <option value="F"> Une Femme </option>
+                            <option value="TF"> une Transféminine</option>
+                            <option value="TH"> une Transmasculin</option>
+                            <option value="B">Bigenre</option>
+                          </Form.Control>
+                        </Form.Group>
+                        <Form.Group as={Col} md="6">
+                          <Form.Label>Je cherche </Form.Label>
+                          <Form.Control
+                            as="select"
+                            onChange={handleChange("sexualPreference")}
+                          >
+                            <option value="N"> Séléctionnez un genre</option>
+                            <option value="M"> Un Homme </option>
+                            <option value="F"> Une Femme </option>
+                            <option value="TF"> une Transféminine</option>
+                            <option value="TH"> une Transmasculin</option>
+                            <option value="B">Bigenre</option>
+                          </Form.Control>
+                        </Form.Group>
+                      </Form.Row>
+                      <Form.Group md="12">
+                        <Form.Label>Adresse postale</Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="Adresse"
+                          name="Adresse"
+                          onChange={handleChange("adress")}
+                        />
+                      </Form.Group>
+                      <Form.Row>
+                        <Form.Group as={Col} md="6">
+                          <Form.Label>ville</Form.Label>
+                          <Form.Control
+                            type="text"
+                            placeholder="ville"
+                            name="ville"
+                            onChange={handleChange("city")}
+                          />
+                        </Form.Group>
+                        <Form.Group as={Col} md="6">
+                          <Form.Label>Code postal</Form.Label>
+                          <Form.Control
+                            type="text"
+                            placeholder="Code postale"
+                            name="code postale"
+                            onChange={handleChange("postalCode")}
+                          />
+                        </Form.Group>
+                      </Form.Row>
+                    </Form>
+                  </Col>
+                </Row>
+                <Row style={{ backgroundColor: "white", marginBottom: "60px" }}>
+                  <Col>
+                    <Form>
+                      <Form.Group
+                        controlId="exampleForm.ControlTextarea1"
+                        md="12"
                       >
-                        <option value="N"> Séléctionnez un genre</option>
-                        <option value="M"> Un Homme </option>
-                        <option value="F"> Une Femme </option>
-                        <option value="TF"> une Transféminine</option>
-                        <option value="TH"> une Transmasculin</option>
-                        <option value="B">Bigenre</option>
-                      </Form.Control>
-                    </Form.Group>
-                    <Form.Group as={Col} md="6">
-                      <Form.Label>Je cherche </Form.Label>
-                      <Form.Control
-                        as="select"
-                        onChange={handleChange("sexualPreference")}
-                      >
-                        <option value="N"> Séléctionnez un genre</option>
-                        <option value="M"> Un Homme </option>
-                        <option value="F"> Une Femme </option>
-                        <option value="TF"> une Transféminine</option>
-                        <option value="TH"> une Transmasculin</option>
-                        <option value="B">Bigenre</option>
-                      </Form.Control>
-                    </Form.Group>
-                  </Form.Row>
-                  <Form.Group md="12">
-                    <Form.Label>Adresse postale</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="Adresse"
-                      name="Adresse"
-                      onChange={handleChange("adress")}
-                    />
-                  </Form.Group>
-                  <Form.Row>
-                    <Form.Group as={Col} md="6">
-                      <Form.Label>ville</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="ville"
-                        name="ville"
-                        onChange={handleChange("city")}
-                      />
-                    </Form.Group>
-                    <Form.Group as={Col} md="6">
-                      <Form.Label>Code postal</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="Code postale"
-                        name="code postale"
-                        onChange={handleChange("postalCode")}
-                      />
-                    </Form.Group>
-                  </Form.Row>
-                  <Form.Group controlId="exampleForm.ControlTextarea1" md="12">
-                    <Form.Label>A propos de vous</Form.Label>
-                    <Form.Control
-                      as="textarea"
-                      rows="4"
-                      placeholder="A propos de vous"
-                      name="bio"
-                      onChange={handleChange("description")}
-                    />
-                  </Form.Group>
-                  <Form.Group>
-                    <Form.Label>Centres d'intérêt</Form.Label>
-                    <Form.Control
-                      onKeyPress={handlePress}
-                      type="text"
-                      placeholder="Tags"
-                      name="myTags"
-                    />
-                    <div className="mytags-main mt-2">
-                      {values.myTags.map((tag, i) => {
-                        return (
-                          <div className=" mytags mr-2 pl-2 mt-2" key={i}>
-                            <div
-                              dangerouslySetInnerHTML={{ __html: "#" + tag }}
-                            />
-                            <Badge
-                              className="badge-delete"
-                              onClick={handleDeleteTag(i)}
-                            >
-                              x
-                            </Badge>
-                          </div>
-                        );
-                      })}
-                    </div>
-                  </Form.Group>
-                  <Button onClick={() => handleSubmit(values)}>Valider</Button>
-                </Form>
+                        <Form.Label>A propos de vous</Form.Label>
+                        <Form.Control
+                          as="textarea"
+                          rows="4"
+                          placeholder="A propos de vous"
+                          name="bio"
+                          onChange={handleChange("description")}
+                        />
+                      </Form.Group>
+                      <Form.Group>
+                        <Form.Label>Centres d'intérêt</Form.Label>
+                        <Form.Control
+                          onKeyPress={handlePress}
+                          type="text"
+                          placeholder="Tags"
+                          name="myTags"
+                        />
+                        <div className="mytags-main mt-2">
+                          {values.myTags.map((tag, i) => {
+                            return (
+                              <div className=" mytags mr-2 pl-2 mt-2" key={i}>
+                                <div
+                                  dangerouslySetInnerHTML={{
+                                    __html: "#" + tag
+                                  }}
+                                />
+                                <Badge
+                                  className="badge-delete"
+                                  onClick={handleDeleteTag(i)}
+                                >
+                                  x
+                                </Badge>
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </Form.Group>
+                      <Button onClick={() => handleSubmit(values)}>
+                        Valider
+                      </Button>
+                    </Form>
+                  </Col>
+                </Row>
               </Col>
             </Row>
           </Col>
