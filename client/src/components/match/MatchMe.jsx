6e96de819b94775fb9322a84656b1fc3e917d7ef
Wafae RHARRABTI <warharra@e2r9p15.42.fr>
2020-01-31 18:00:03 +0100

    [WIP] MatchMe

--- client/src/components/match/MatchMe.jsx
+++ client/src/components/match/MatchMe.jsx
@@ -3,11 +3,16 @@ import "../profile/CardPicture.css";
 import "../profile/CardPicture";
 import { Row, Col } from "react-bootstrap";
 import CardPicture from "../profile/CardPicture";
+import NavbarHeader from "../navbar/Navbar";
+import "./MatchMe.css";
+
+// import NWReactSlider from "nw-react-slider";
 
 const MatchMe = ({ pseudo, lastName, city, birthday }) => {
   const [values, setValues] = useState({
     image: [],
-    uploading: false
+    uploading: false,
+    value: { min: 2, max: 10 }
   });
   const handleChange = event => {
     event.preventDefault();
@@ -28,63 +33,164 @@ const MatchMe = ({ pseudo, lastName, city, birthday }) => {
   };
   return (
     <Fragment>
+      <NavbarHeader />
+      {/* <Container> */}
       <Row>
-        <Col>
-          <CardPicture />
-        </Col>{" "}
-        <Col>
-          <CardPicture />
+        <Col md={4} className="pl-5">
+          <Row className="style_menu mt-5">
+            <Col></Col>
+          </Row>
         </Col>
-        <Col>
-          <CardPicture />
-        </Col>
-        <Col>
-          <CardPicture />
-        </Col>
-        <Col>
-          <CardPicture />
-        </Col>
-        <Col>
-          <CardPicture />
-        </Col>
-        <Col>
-          <CardPicture />
-        </Col>
-        <Col>
-          <CardPicture />
-        </Col>
-        <Col>
-          <CardPicture />
-        </Col>
-        <Col>
-          <CardPicture />
-        </Col>
-        <Col>
-          <CardPicture />
-        </Col>
-        <Col>
-          <CardPicture />
-        </Col>
-        <Col>
-          <CardPicture />
-        </Col>
-        <Col>
-          <CardPicture />
-        </Col>
-        v
-        <Col>
-          <CardPicture />
-        </Col>
-        <Col>
-          <CardPicture />
-        </Col>
-        <Col>
-          <CardPicture />
-        </Col>
-        <Col>
-          <CardPicture />
+        <Col md={8} className="pl-5 pr-5">
+          <Row className="style_row mt-5">
+            <Col>
+              <CardPicture
+                lastName="Pier'Antonio"
+                pseudo="Ppichier"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>{" "}
+            <Col>
+              <CardPicture
+                lastName="Wafae"
+                pseudo="Warharra"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Jules"
+                pseudo="Jumourot"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Judith"
+                pseudo="Jpoulvel"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Myriam"
+                pseudo="Manki"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Edd"
+                pseudo="Ylaissyi"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Mohammed"
+                pseudo="Moslama"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Valentin"
+                pseudo="Vrossi"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Alexandra"
+                pseudo="Perrin"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Yacine"
+                pseudo="Ychair"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Stephen"
+                pseudo="Stelie"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Fadia"
+                pseudo="Zfadia"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Mathieu"
+                pseudo="Mjouffro"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Max"
+                pseudo="Maloua-h"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Flavien"
+                pseudo="Fhenrion"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Valentin"
+                pseudo="Valecar"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Carole"
+                pseudo="Ccarole"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+            <Col>
+              <CardPicture
+                lastName="Marie Lise"
+                pseudo="Picard"
+                city="Paris"
+                birthday="05/18/1992"
+              />
+            </Col>
+          </Row>
         </Col>
       </Row>
+      {/* </Container> */}
     </Fragment>
   );
 };
