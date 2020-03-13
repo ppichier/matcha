1e9a9e6c142371a56e96de69d5d523cc87d9430f
Pichierri <pierantonio.pichierri@gmail.com>
2020-03-13 09:52:07 +0100

    [CLEAN]

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import Routes from "./Routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import "../node_modules/font-awesome/css/font-awesome.min.css";
 
-// ReactDOM.render(<Routes />, document.getElementById("root"));
+ReactDOM.render(<Routes />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
